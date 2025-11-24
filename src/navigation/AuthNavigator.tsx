f8c51681a4a2d94b7e1aeb499b120a5687891a03
Joshypeace <joshuachilapondwa2@gmail.com>
2025-11-24 05:00:38 +0200

    added proper folder accessing

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -1,9 +1,9 @@
 // AuthNavigator.tsx
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from '../screens/auth/LoginScreen';
-import RegisterScreen from '../screens/auth/RegisterScreen';
-import ForgotPasswordScreen from '../screens/auth/ForgotPasswordScreen';
+import LoginScreen from '../app/(auth)/login';
+import RegisterScreen from '../app/(auth)/register';
+import ForgotPasswordScreen from '../app/(auth)/forgot-password';
 
 const Stack = createNativeStackNavigator();
 
